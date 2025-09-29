59662a7d504c7161b94800b2b950c7116a2dfec4
salcedof <salcedomarcof91@gmail.com>
2025-09-29 17:06:08 -0600

    correcting code

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
