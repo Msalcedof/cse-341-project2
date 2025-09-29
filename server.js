460651629909e4bd78aa18cabf40aae9cc8fa630
salcedof <salcedomarcof91@gmail.com>
2025-09-29 14:17:33 -0600

    correcting issues with mongo

--- server.js
+++ server.js
@@ -14,13 +14,9 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log(' Connected to MongoDB'))
-  .catch(err => console.error(' MongoDB connection error:', err));
-
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port http://localhost:${process.env.PORT}`);
