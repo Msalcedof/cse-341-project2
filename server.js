5d8ae3d29999418e8529761dfd3792adf380f084
salcedof <salcedomarcof91@gmail.com>
2025-10-11 23:29:38 -0600

    fixing dbs

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: 'cse-341-Project2',
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
